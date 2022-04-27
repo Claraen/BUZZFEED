d924d71b0a47c854d37001e932c10b62796c3866
Clara Cecilia Elm Nettesheim <95877025+Claraen@users.noreply.github.com>
2022-04-27 03:53:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// HELLO!
+//pain = life
+
+// you still joined though!
+//what should it be about? var life ++ pain
+// life == false;
+// what should the thing bee about;
+//answer 
+//sage you better answer! = bees!!
+// also veronika
+// tell us you favoirte deserts and well tell you what bee you are
+// beez
+// favorite deserts ?
+
+//I have forgotten all JS syntax
+// it prints bee
+// sage can you find some bee pictures?
+//true
+bee();
+var questNum = 0;
+var scoreCount = 0;
+
+function bee(){
+  var bee = true;
+  if(bee){
+    console.log("bee");
+  }
+}
+
+function newSet(){
+  
+}
+//climate?
+//place to live? ie log, underground, with roommates
